f2b989cdd68e8c69ac5e7cfe66e0627c2c933a48
goldstargreat <goldstargreat@gmail.com>
2026-05-21 14:51:30 +0900

    옵션메뉴 적용

--- app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/menutest1/MainActivity.java
@@ -1,17 +1,29 @@
 package kr.ac.kopo.menutest1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    LinearLayout linear;
+    Button btn;
+    float rotationDegree = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +32,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        linear = findViewById(R.id.main);
+        btn = findViewById(R.id.btn);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.item_yellow)
+        {
+            linear.setBackgroundColor(Color.YELLOW);
+            return true;
+        } else if (item.getItemId() == R.id.item_khaki)
+        {
+            linear.setBackgroundColor(Color.rgb(240, 230, 140));
+            return true;
+        } else if (item.getItemId() == R.id.item_blue)
+        {
+            linear.setBackgroundColor(Color.BLUE);
+            return true;
+        } else if (item.getItemId() == R.id.btn_rotate)
+        {
+            rotationDegree += 45;
+            btn.setRotation(rotationDegree);  // +45도 누적 회전
+            return true;
+        } else if (item.getItemId() == R.id.btn_zoomin)
+        {
+            btn.setScaleX(2);
+            return true;
+        } else if (item.getItemId() == R.id.btn_rotate_neg)
+        {
+            rotationDegree -= 45;
+            btn.setRotation(rotationDegree);  // -45도 누적 회전
+            return true;
+        } else if (item.getItemId() == R.id.btn_zoomin_y)
+        {
+            btn.setScaleY(2);
+            return true;
+        }
+        return false;
     }
 }
